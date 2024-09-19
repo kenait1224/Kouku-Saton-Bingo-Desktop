1533827bf9700993853b803924550d0195dd9a6a
kenny <kenny224455@gmail.com>
2024-09-19 11:33:47 +0800

    Update version information

--- Kouku-Saton-Bingo/Properties/AssemblyInfo.cs
+++ Kouku-Saton-Bingo/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GLHF")]
 [assembly: AssemblyProduct("Kouku-Saton-Bingo")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2024  kenait1224")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
